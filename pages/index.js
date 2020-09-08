358d8cb92eda0d9ed9754ff291375a0b8bc03389
Nolan Phillips <ncphillips.19@gmail.com>
2020-09-08 16:03:13 -0300

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,53 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
+import Container from '../components/container';
+import Intro from '../components/intro';
+import Layout from '../components/layout';
+import { getAllPosts } from '../lib/api';
+import Head from 'next/head';
+import { CMS_NAME } from '../lib/constants';
+import Link from 'next/link';
 
 export default function Index({ allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
   return (
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>TinaCMS Media Manager Prototype</title>
         </Head>
         <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          <section className='flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12'>
+            <h1 className='text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8'>
+              TinaLabs
+            </h1>
+            <h4 className='text-center md:text-left text-lg mt-5 md:pl-8'>
+              A site used to prototype the TinaCMS Media Manager.
+            </h4>
+          </section>
+          <ul>
+            <li>
+              <Link href='/gh'>
+                <a>GitHub </a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/strapi'>
+                <a>Strapi </a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/contentful'>
+                <a>Contentful </a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/git'>
+                <a>Git </a>
+              </Link>
+            </li>
+          </ul>
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps() {
@@ -43,9 +58,9 @@ export async function getStaticProps() {
     'author',
     'coverImage',
     'excerpt',
-  ])
+  ]);
 
   return {
     props: { allPosts },
-  }
+  };
 }
