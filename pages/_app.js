69d7ac93efb51afea88380c228ae41b4cf68ae33
DJ <dj@walker.dev>
2020-09-08 14:49:46 -0400

    add tina

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/index.css'
+import { TinaProvider, TinaCMS } from 'tinacms'
+
+const cms = new TinaCMS({ enabled: true, sidebar: true })
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <TinaProvider cms={cms}>
+      <Component {...pageProps} />
+    </TinaProvider>
+  )
 }
