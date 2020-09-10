9b5feb17eb8c4d31b100df794ded5eb4fad986b5
Nolan Phillips <ncphillips.19@gmail.com>
2020-09-10 16:31:57 -0300

    webpack helpe

--- next.config.js
+++ next.config.js
@@ -4,5 +4,10 @@ module.exports = {
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     REPO_FULL_NAME: process.env.REPO_FULL_NAME,
     BASE_BRANCH: process.env.BASE_BRANCH,
-  }
+  },
+  webpack(config) {
+    if (process.env.TINA) {
+      aliasTinaDev(config, process.env.TINA)
+    }
+  },
 }
